18633ea79189dc541ba72f78b81cd574f466a044
Daniel Hideki <daniel.hideki@builtcode.com.br>
2021-11-03 23:42:07 -0300

    Implementado a classe Domain, suas entidades, suas RepositoryInterface, sua validação e seus testes

---
+++ Clean-Architecture.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Clean_Architecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Architecture.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ Clean-Architecture.Domain/Validation/DomainExecptionValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Architecture.Domain.Validation
+{
+    public class DomainExecptionValidation : Exception
+    {
+        public DomainExecptionValidation(string error) : base(error)
+        {}
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+                throw new DomainExecptionValidation(error);
+        }
+    }
+}
